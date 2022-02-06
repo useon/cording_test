84f888125730fa5ffbfb2fa68cb60bc1f9324ded
useon <yoosk4986@naver.com>
2022-02-06 23:57:59 +0900

    📝 220206 Level1 23번

---
+++ 프로그래머스 코딩테스트 JS/1단계/23. 서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+  let answer = '';
+  answer = '김서방은 ' + seoul.indexOf("Kim") + '에 있다'
+  return answer;
+}
+
+// test
+// let seoul = ["Jane", "Kim"];
+// solution(seoul);
+// 1
