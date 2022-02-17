bc503cbd746f28fe93bafac31ab9df3db4f1e504
useon <yoosk4986@naver.com>
2022-02-17 22:54:03 +0900

    📝 220217 Level1 26번

---
+++ 프로그래머스 코딩테스트 JS/1단계/26. 약수의 합.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let answer = 0;
+  for (let i = 1; i < n + 1; i++) {
+    if (n % i === 0) {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+// test
+// let n = 3000;
+// solution(n)
+// 4
