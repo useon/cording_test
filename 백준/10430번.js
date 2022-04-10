3f7bf1015ad226d9e3ba20babea5fa53f6a77e3b
김유선 (Yuseon Kim) <74897720+useon@users.noreply.github.com>
2022-04-10 15:33:54 +0900

    10430번

---
+++ 백준/10430번.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split(' ').map((x) => +x);
+const a = input[0];
+const b = input[1];
+const c = input[2];
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
