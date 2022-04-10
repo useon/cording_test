f10b85a0fe17a81172f0a0da326b851d27c76182
김유선 (Yuseon Kim) <74897720+useon@users.noreply.github.com>
2022-04-10 15:52:43 +0900

    1330번
    2단계 조건문: 1번 두 수 비교하기

---
+++ 백준/1330번.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split(' ').map((x) => +x);
+
+const a = input[0];
+const b = input[1];
+
+if (a > b) {
+  console.log('>');
+} else if (a < b) {
+  console.log('<');
+} else {
+  console.log('==');
+}
