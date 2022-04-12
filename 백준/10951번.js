f302f7829a50a223655e827dbd03482e56ddad92
김유선 (Yuseon Kim) <74897720+useon@users.noreply.github.com>
2022-04-12 11:32:36 +0900

    10951번
    3단계 반복문
    13번 A+B - 4

---
+++ 백준/10951번.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().replace(/\r/g, '').split('\n');
+for (let i = 0; i < input.length; i++) {
+  let a = Number(input[i][0]);
+  let b = Number(input[i][2]);
+  console.log(a + b);
+}
